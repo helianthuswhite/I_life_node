f79b627802724c57ba17fd1377ef03ead2f9a0f8
Littlewhite <964873559@qq.com>
2016-09-28 11:45:37 +0800

    修改结构并添加node调用

---
+++ Node_Jar/node_jar.js
@@ -0,0 +1,16 @@
+
+//获取到创建进程的方法
+const spawn = require('child_process').spawn;
+const ls = spawn('java', ['-jar', '/Users/W_littlewhite/Documents/Git/Node_Opencv/Node_Jar/test.jar']);
+
+ls.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+ls.stderr.on('data', (data) => {
+  console.log(`stderr: ${data}`);
+});
+
+ls.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
