eef0402220a5033566fdfd9a113f01cf145bebf1
Littlewhite <964873559@qq.com>
2016-10-06 19:15:45 +0800

    文件上传

--- Upload_Image/app.js
+++ Upload_Image/app.js
@@ -1,40 +1,62 @@
-//引入express框架
-var express = require('express');
-var app = express();
-var formidable = require('formidable');
+var express = require('express')
+  router = express.Router(),	
+  formidable = require('formidable'),
+    fs = require('fs'),
+    TITLE = 'formidable上传示例',
+  AVATAR_UPLOAD_FOLDER = '/avatar/'
 
-app.get('/',function (req,res) {
-	res.sendFile(__dirname + '/index.html');
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: TITLE });
 });
 
-app.post('/upload', function(req, res) {
+router.post('/', function(req, res) {
 
-  	var form = new formidable.IncomingForm();   //创建上传表单
+  var form = new formidable.IncomingForm();   //创建上传表单
     form.encoding = 'utf-8';		//设置编辑
-    form.uploadDir = __dirname;	 //设置上传目录
+    form.uploadDir = 'public' + AVATAR_UPLOAD_FOLDER;	 //设置上传目录
     form.keepExtensions = true;	 //保留后缀
     form.maxFieldsSize = 2 * 1024 * 1024;   //文件大小
 
-	form.parse(req, function(err, fields, files) {
+  form.parse(req, function(err, fields, files) {
 
-	    if (err) {
-	      res.locals.error = err;
-	      return;		
-	    }  
+    if (err) {
+      res.locals.error = err;
+      res.render('index', { title: TITLE });
+      return;		
+    }  
+     
+    var extName = '';  //后缀名
+    switch (files.fulAvatar.type) {
+      case 'image/pjpeg':
+        extName = 'jpg';
+        break;
+      case 'image/jpeg':
+        extName = 'jpg';
+        break;		 
+      case 'image/png':
+        extName = 'png';
+        break;
+      case 'image/x-png':
+        extName = 'png';
+        break;		 
+    }
 
-	    var newPath = form.uploadDir + '.jpg';
+    if(extName.length == 0){
+        res.locals.error = '只支持png和jpg格式图片';
+        res.render('index', { title: TITLE });
+        return;				   
+    }
 
-	    console.log(newPath);
-	    // fs.renameSync(files.fulAvatar.path, newPath);  //重命名
-	});
+    var avatarName = Math.random() + '.' + extName;
+    var newPath = form.uploadDir + avatarName;
 
-  	res.locals.success = '上传成功';
+    console.log(newPath);
+    fs.renameSync(files.fulAvatar.path, newPath);  //重命名
+  });
+
+  res.locals.success = '上传成功';
+  res.render('index', { title: TITLE });	  
 });
 
-//服务器启动
-var server = app.listen(2333,function () {
-	var host = server.address().address;
-	var port = server.address().port;
-
-	console.log('应用实例，访问地址为http://%s:%s',host,port);
-});
+module.exports = router;
